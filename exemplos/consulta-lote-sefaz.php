025c45efee80233fe6f5c8051bdfd7798434f78f
Marabesi <mmarabesi@2mundos.net>
2016-10-18 22:57:32 -0200

    Adicionado modo debug

--- exemplos/consulta-lote-sefaz.php
+++ exemplos/consulta-lote-sefaz.php
@@ -5,70 +5,70 @@ require '../vendor/autoload.php';
 class MySetup extends Sped\Gnre\Configuration\Setup {
 
     public function getBaseUrl() {
-        
+
     }
 
     public function getCertificateCnpj() {
-        
+
     }
 
     public function getCertificateDirectory() {
-        
+
     }
 
     public function getCertificateName() {
-        
+
     }
 
     public function getCertificatePassword() {
-        
+
     }
 
     public function getCertificatePemFile() {
-        
+
     }
 
     public function getEnvironment() {
-        
+
     }
 
     public function getPrivateKey() {
-        
+
     }
 
     public function getProxyIp() {
-        
+
     }
 
     public function getProxyPass() {
-        
+
     }
 
     public function getProxyPort() {
-        
+
     }
 
     public function getProxyUser() {
-        
-    }
 
-    public function getDebug()
-    {
-        return true;
     }
 }
-
-$xml = file_get_contents('estrutura-lote-completo-gnre.xml');
-
 
 $minhaConfiguracao = new MySetup();
 
 $guia = new Sped\Gnre\Sefaz\Guia();
 
-$lote = new Sped\Gnre\Sefaz\Lote();
-// $lote->utilizarAmbienteDeTeste(true); Descomente essa linha para utilizar o ambiente de testes
+$consulta = new Sped\Gnre\Sefaz\Consulta();
+$consulta->setRecibo(12345123);
 
-$lote->addGuia($guia);
+/**
+ * O número que representa em qual ambiente sera realizada a consulta
+ * 1 - produção 2 - homologação
+ */
+$consulta->setEnvironment(1);
+//$consulta->utilizarAmbienteDeTeste(true); //Descomente essa linha para utilizar o ambiente de testes
 
-$webService = new Sped\Gnre\Webservice\Connection($minhaConfiguracao, $lote->getHeaderSoap(), $lote->toXml());
-echo $webService->doRequest($lote->soapAction());
+//header('Content-Type: text/xml');
+//print $consulta->toXml(); // exibe o XML da consulta
+
+$webService = new Sped\Gnre\Webservice\Connection($minhaConfiguracao, $consulta->getHeaderSoap(), $consulta->toXml());
+echo $webService->doRequest($consulta->soapAction());
