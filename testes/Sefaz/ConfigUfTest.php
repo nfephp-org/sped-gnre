20fb7af58a89e651610dfe4ae689f61117113e48
Renan Delmonico <renandelmonico@gmail.com>
2017-04-22 13:50:29 -0300

    Adicionados os testes para a consulta das configuracoes das UFs

---
+++ testes/Sefaz/ConfigUfTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @covers Sped\Gnre\Sefaz\ConfigUf
+ */
+class ConfigUfTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDeveRetornarOsCabecalhosParaArequisicaoSoap()
+    {
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+        $headersArray = $consulta->getHeaderSoap();
+
+        $this->assertEquals('Content-Type: application/soap+xml;charset=utf-8;action="http://www.gnre.pe.gov.br/webservice/GnreConfigUF"', $headersArray[0]);
+        $this->assertEquals('SOAPAction: consultar', $headersArray[1]);
+    }
+
+    public function testDeveRetornarOsCabecalhosParaArequisicaoSoapAoWebserviceDeTestes()
+    {
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+        $consulta->utilizarAmbienteDeTeste(true);
+
+        $headersArray = $consulta->getHeaderSoap();
+
+        $this->assertEquals('Content-Type: application/soap+xml;charset=utf-8;action="http://www.testegnre.pe.gov.br/webservice/GnreConfigUF"', $headersArray[0]);
+        $this->assertEquals('SOAPAction: consultar', $headersArray[1]);
+    }
+
+    public function testDeveRetornarAacaoAserExecutadaNoSoap()
+    {
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+
+        $this->assertEquals('https://www.gnre.pe.gov.br/gnreWS/services/GnreConfigUF', $consulta->soapAction());
+    }
+
+    public function testDeveRetornarXmlCompletoVazioParaRealizarAconsulta()
+    {
+        $dadosParaConsulta = file_get_contents(__DIR__ . '/../../exemplos/envelope-consulta-config-uf.xml');
+        
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+        $consulta->setEnvironment(1);
+        $consulta->setEstado('PR');
+        $consulta->setReceita(100099);
+
+        $this->assertXmlStringEqualsXmlString($dadosParaConsulta, $consulta->toXml());
+    }
+
+    public function testDeveRetornarAactionAserExecutadaNoWebServiceDeProducao()
+    {
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+
+        $this->assertEquals($consulta->soapAction(), 'https://www.gnre.pe.gov.br/gnreWS/services/GnreConfigUF');
+    }
+
+    public function testDeveRetornarAactionAserExecutadaNoWebServiceDeTestes()
+    {
+        $consulta = new Sped\Gnre\Sefaz\ConfigUf();
+        $consulta->utilizarAmbienteDeTeste(true);
+
+        $this->assertEquals($consulta->soapAction(), 'https://www.testegnre.pe.gov.br/gnreWS/services/GnreConfigUF');
+    }
+    
+}
---
+++ testes/Sefaz/ConsultaConfigUfTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Sped\Gnre\Sefaz\Test;
+
+use Sped\Gnre\Sefaz\boolen;
+use Sped\Gnre\Sefaz\ConsultaConfigUf;
+
+/**
+ * @covers Sped\Gnre\Sefaz\ConsultaConfigUf
+ */
+class ConsultaConfigUfTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDeveDefinirAreceitaParaSerUtilizada()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $this->assertNull($gnreConsulta->setReceita(100099));
+    }
+
+    public function testDeveRetornarAreceitaDefinida()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $gnreConsulta->setReceita(100099);
+
+        $this->assertEquals(100099, $gnreConsulta->getReceita());
+    }
+
+    public function testDeveDefinirOestadoParaSerUtilizado()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $this->assertNull($gnreConsulta->setEstado('PR'));
+    }
+
+    public function testDeveRetornarOestadoDefinido()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $gnreConsulta->setEstado('PR');
+
+        $this->assertEquals('PR', $gnreConsulta->getEstado());
+    }
+
+    public function testDeveDefinirOambienteParaSerUtilizado()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $this->assertNull($gnreConsulta->setEnvironment(1));
+    }
+
+    public function testDeveRetornarOambienteDefinido()
+    {
+        $gnreConsulta = new MinhaConsultaConfigUf();
+        $gnreConsulta->setEnvironment(1);
+
+        $this->assertEquals(1, $gnreConsulta->getEnvironment());
+    }
+
+}
+
+class MinhaConsultaConfigUf extends ConsultaConfigUf
+{
+
+    public function getHeaderSoap()
+    {
+        
+    }
+
+    public function soapAction()
+    {
+        
+    }
+
+    public function toXml()
+    {
+        
+    }
+
+    public function getSoapEnvelop($noRaiz, $conteudoEnvelope)
+    {
+
+    }
+
+    public function utilizarAmbienteDeTeste($ambiente = false)
+    {
+
+    }
+}
