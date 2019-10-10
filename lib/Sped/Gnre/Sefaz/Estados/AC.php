cca598208f21fef8e4caa89ca2f26ab0b4e3381c
Matheus Marques <matheusocmarques@gmail.com>
2019-10-10 09:53:23 -0300

    Permite campos extras para AC

--- lib/Sped/Gnre/Sefaz/Estados/AC.php
+++ lib/Sped/Gnre/Sefaz/Estados/AC.php
@@ -7,15 +7,4 @@ use Sped\Gnre\Sefaz\Guia;
 class AC extends Padrao
 {
 
-
-    /**
-     * Estado do Acre não possui campos extras. Com esse método mesmo que
-     * seja informado qualquer campo extra na $gnreGuia não vamos adicionar
-     * no XML
-     * {@inheritdoc}
-     */
-    public function getNodeCamposExtras(\DOMDocument $gnre, Guia $gnreGuia)
-    {
-        return null;
-    }
 }
