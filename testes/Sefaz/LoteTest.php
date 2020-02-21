14888f5418d62a5360f0c29b9f7b43bb8fbf9c8d
marabesi <matheus.marabesi@gmail.com>
2020-02-21 21:46:26 +0100

    criado novo test case: lote para emitente físico e sem telefone

--- testes/Sefaz/LoteTest.php
+++ testes/Sefaz/LoteTest.php
@@ -161,6 +161,47 @@ class LoteTest extends TestCase
         $this->assertXmlStringEqualsXmlString($estruturaLote, $lote->toXml());
     }
 
+    public function testDeveRetornarOxmlDoLoteSemOcampoTelefoneEmitenteParaEmitenteEdestinatarioFisicos()
+    {
+        $estruturaLote = file_get_contents(__DIR__ . '/../../exemplos/xml/lote-emit-cpf-dest-cpf-sem-telefone-emitente.xml');
+
+        $guia = new Guia();
+
+        $guia->c01_UfFavorecida = 26;
+        $guia->c02_receita = 1000099;
+        $guia->c25_detalhamentoReceita = 10101010;
+        $guia->c26_produto = 'TESTE DE PROD';
+        $guia->c27_tipoIdentificacaoEmitente = 2;
+        $guia->c03_idContribuinteEmitente = 52162197650;
+        $guia->c28_tipoDocOrigem = 10;
+        $guia->c04_docOrigem = 5656;
+        $guia->c06_valorPrincipal = 10.99;
+        $guia->c10_valorTotal = 12.52;
+        $guia->c14_dataVencimento = '2015-05-01';
+        $guia->c15_convenio = 546456;
+        $guia->c16_razaoSocialEmitente = 'GNRE PHP EMITENTE';
+        $guia->c17_inscricaoEstadualEmitente = 56756;
+        $guia->c18_enderecoEmitente = 'Queens St';
+        $guia->c19_municipioEmitente = 5300108;
+        $guia->c20_ufEnderecoEmitente = 'DF';
+        $guia->c21_cepEmitente = '08215917';
+        $guia->c34_tipoIdentificacaoDestinatario = 2;
+        $guia->c35_idContribuinteDestinatario = 99942896759;
+        $guia->c36_inscricaoEstadualDestinatario = 10809181;
+        $guia->c37_razaoSocialDestinatario = 'RAZAO SOCIAL GNRE PHP DESTINATARIO';
+        $guia->c38_municipioDestinatario = 2702306;
+        $guia->c33_dataPagamento = '2015-11-30';
+        $guia->mes = '05';
+        $guia->ano = 2015;
+        $guia->parcela = 2;
+        $guia->periodo = 2014;
+
+        $lote = new Lote();
+        $lote->addGuia($guia);
+
+        $this->assertXmlStringEqualsXmlString($estruturaLote, $lote->toXml());
+    }
+
     public function testDeveRetornarOxmlDoLoteComOsCamposExtras()
     {
         $estruturaLote = file_get_contents(__DIR__ . '/../../exemplos/estrutura-lote-completo-gnre.xml');
