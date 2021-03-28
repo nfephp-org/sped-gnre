4fdaa791a128b45371214b9b924a99efd35d4d50
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-03-28 19:47:07 +0200

    criado index.php para os exemplos disponíveis no projeto

---
+++ exemplos/index.php
@@ -0,0 +1,24 @@
+<p>
+    <a href="gerar-xml.php">Gerar XML</a>
+</p>
+<p>
+    <a href="gerar-pdf.php">Gerar PDF</a>
+</p>
+<p>
+    <a href="tratar-retorno-sefaz.php">Tratar retorno sefaz</a>
+</p>
+<p>
+    <a href="gerar-codigo-de-barras.php">Gerar código de barras</a>
+</p>
+<p>
+    <a href="consulta-config-uf.php">Consultar configuração UF</a>
+</p>
+<p>
+    <a href="consulta-lote-sefaz.php">Consultar lote sefaz</a>
+</p>
+<p>
+    <a href="enviar-lote-sefaz.php">Enviar lote sefaz</a>
+</p>
+<p>
+    <a href="extrair-dados-certificado.php">Extrair dados certificado</a>
+</p>
